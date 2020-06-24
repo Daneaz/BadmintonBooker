aa4f48b36d559ebe845f839e9b679591b5438291
Eugene Wu WeiJie <eugeneaad@gmail.com>
2020-06-25 01:45:32 +0800

    fix bugs

--- BadmintonBooking.js
+++ BadmintonBooking.js
@@ -3,14 +3,17 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'result'), (err) => {
+
+let date = "05/07/2020";
+let resultPath = `result/${date.split('/').join("")}`
+fs.mkdir(path.join(__dirname, resultPath), (err) => {
     if (err) {
         return console.error(err);
     }
     console.log('Directory created successfully!');
 });
 // dd/mm/yyyy
-let date = "07/07/2020";
+
 (async () => {
     const width = 1980;
     const height = 1080;
@@ -37,23 +40,18 @@ let date = "07/07/2020";
         await locationPicker.click();
         if (i != 0)
             await page.keyboard.press('ArrowDown');
-        await delay(100)
+        await delay(50)
         await page.keyboard.press('Enter');
         await page.waitForNavigation();
-        try {
-            let locationName = await page.$eval('.location', el => el.innerHTML)
-            locationName = locationName.trim();
-            console.log(`Searching for ${locationName}`)
-            await page.$eval('.slots.normal', el => el.value);
+        let locationName = await page.$eval('.location', el => el.innerHTML)
+        locationName = locationName.trim();
+        console.log(`Searching for ${locationName}`)
+        if (await searchForSlot(page, "peak") || await searchForSlot(page, "normal")) {
             let screenshot = await page.$('#facTable1')
-            await screenshot.screenshot({ path: `result/${locationName}.png`, type: "png" });
+            await screenshot.screenshot({ path: `${resultPath}/${locationName}.png`, type: "png" });
             console.log(`Slot found for ${locationName}`);
             count++;
             resultList.push(locationName);
-            await delay(2000)
-        } catch (err) {
-            if (err.message !== `Error: failed to find element matching selector ".slots.normal"`)
-                console.log(err)
         }
     }
 
@@ -62,10 +60,24 @@ let date = "07/07/2020";
     resultList.map(result => {
         console.log(result);
     })
-    console.log(`There are total ${count} CC with slot`)
+    if (count === 0)
+        console.log("There is no slots available")
+    else
+        console.log(`There are total ${count} CC with slot`)
 })();
 
-
+async function searchForSlot(page, type) {
+    try {
+        await page.$eval(`.slots.${type}`, el => el.value);
+        return true
+    } catch (err) {
+        if (err.message === `Error: failed to find element matching selector ".slots.normal"` || err.message === `Error: failed to find element matching selector ".slots.peak"`) {
+            return false
+        } else {
+            console.log(err)
+        }
+    }
+}
 
 function delay(time) {
     return new Promise(function (resolve) {
